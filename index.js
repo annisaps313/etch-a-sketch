126f30716429d8b0d4463805f451f6350f9628fa
annisaps313 <annisaps313@gmail.com>
2026-03-15 18:04:38 +0700

    Add mousemove

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ container.appendChild(div);
 div.style.width = "16px";
 div.style.height = "16px";
 div.style.background = "beige";
+
+div.addEventListener("mousemove", ()=>{
+    div.style.background = "grey";
+})
