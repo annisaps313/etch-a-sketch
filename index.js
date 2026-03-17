bc4fb520e09a141d4313ca651c43196ceb309842
annisaps313 <annisaps313@gmail.com>
2026-03-17 12:56:12 +0700

    Fix looping

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let size = 16;
 let containerWidth = container.clientWidth;
 let containerHeight = container.clientHeight;
 
-for (let i = 0; i < containerHeight; i++){
+for (let i = 0; i < containerHeight / size; i++){
     let row = document.createElement("div");
     container.appendChild(row);
     row.style.display = "flex";
-    for (let i = 0; i < containerWidth; i++){
+    for (let i = 0; i < containerWidth / size; i++){
         let grid = document.createElement("div");
         grid.style.width = `${size}px`;
         grid.style.height = `${size}px`;
