8e2ce3b38c1aa73cb3a31234c7027a1bb01f4e54
annisaps313 <annisaps313@gmail.com>
2026-03-17 12:52:47 +0700

    Add flex to row

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let containerHeight = container.clientHeight;
 for (let i = 0; i < containerHeight; i++){
     let row = document.createElement("div");
     container.appendChild(row);
+    row.style.display = "flex";
     for (let i = 0; i < containerWidth; i++){
         let grid = document.createElement("div");
         grid.style.width = `${size}px`;
