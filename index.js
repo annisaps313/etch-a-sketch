1c972fde0c9ceffdff7f8b4774c474ad31f61a16
annisaps313 <annisaps313@gmail.com>
2026-03-17 13:46:04 +0700

    Add custom grid size

--- index.js
+++ index.js
@@ -7,19 +7,28 @@ let size = 16;
 let containerWidth = container.clientWidth;
 let containerHeight = container.clientHeight;
 
-for (let i = 0; i < containerHeight / size; i++){
+function createGrid(size) {
+  for (let i = 0; i < containerHeight / size; i++) {
     let row = document.createElement("div");
     container.appendChild(row);
     row.style.display = "flex";
-    for (let i = 0; i < containerWidth / size; i++){
-        let grid = document.createElement("div");
-        grid.style.width = `${size}px`;
-        grid.style.height = `${size}px`;
-        grid.style.background = "beige";
-        row.appendChild(grid);
+    for (let i = 0; i < containerWidth / size; i++) {
+      let grid = document.createElement("div");
+      grid.style.width = `${size}px`;
+      grid.style.height = `${size}px`;
+      grid.style.background = "beige";
+      row.appendChild(grid);
 
-        grid.addEventListener("mouseover", ()=>{
-            grid.style.background = "grey";
-        });
+      grid.addEventListener("mouseover", () => {
+        grid.style.background = "grey";
+      });
     }
+  }
+}
+
+createGrid(size);
+
+let getSize = ()=>{
+    let input = prompt();
+    return +input;
 }
