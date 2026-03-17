23eef7e897591779513dc892a9c274b72c44fbf3
annisaps313 <annisaps313@gmail.com>
2026-03-17 11:50:30 +0700

    Remove fixed div size

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ let div = document.createElement("div");
 let container = document.querySelector(".container");
 container.appendChild(div);
 
-div.style.width = "16px";
-div.style.height = "16px";
 div.style.background = "beige";
 
 div.addEventListener("mousemove", ()=>{
