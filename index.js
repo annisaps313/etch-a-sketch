9f1ab0fe8cb23e37206eaba0bcaa4dc4d53cd18f
annisaps313 <annisaps313@gmail.com>
2026-03-17 12:11:52 +0700

    Move div style to looping

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 let container = document.querySelector(".container");
 container.appendChild(div);
 
-div.style.background = "beige";
 
-div.addEventListener("mousemove", ()=>{
-    div.style.background = "grey";
-})
+div.addEventListener("mousemove", () => {
+  div.style.background = "grey";
+});
 
 let size = 16;
 
 let btn = document.querySelector("button");
-btn.addEventListener("click", ()=>{
-    size = prompt("Add new size");
-})
+btn.addEventListener("click", () => {
+  size = prompt("Add new size");
+});
 
 console.log(container.clientWidth);
 console.log(container.clientHeight);
 
-for (let i = 0; i < size * size; i++){
-    
+for (let i = 0; i < size * size; i++) {
+  let div = document.createElement("div");
+div.style.background = "beige";
+div.style.width = `${container.clientWidth / size}px`;
 }
